3b97481af071d0521b12e6ce18efb9d575cec5ea
yuseffathi <yuseffathi@gmail.com>
2022-02-14 22:45:11 +0200

    profile edit

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rest_prov/const/colors.dart';
+import 'package:rest_prov/providers/auth.dart';
 import 'package:rest_prov/utils/helper.dart';
 
-import '../providers/restuarant.dart';
 import '../providers/restuarants.dart';
 import 'homepage.dart';
 
@@ -17,146 +19,186 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  @override
-  void initState() {
-    Provider.of<Restaurants>(context, listen: false).fetchRestaurant();
+  Map m, _m;
+  String emaild = "null";
+  Future<void> getData() async {
+    await Provider.of<Auth>(context, listen: false).userData().then((value) =>
+        emaild = Provider.of<Auth>(context, listen: false).emaildata);
+    await Provider.of<Restaurants>(context, listen: false)
+        .fetchRestaurant()
+        .then((value) =>
+            _m = Provider.of<Restaurants>(context, listen: false).rest);
+    setState(() {
+      m = _m;
+    });
   }
 
-  Widget build(BuildContext context) {
-    //final restaurant =
-    final rest = Provider.of<Restaurants>(context, listen: false).rest;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
-    final _restaurant = ModalRoute.of(context).settings.arguments as Restaurant;
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Image imageFromBase64String(String base64String) {
+    return Image.memory(base64Decode(base64String));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final emaild = Provider.of<Auth>(context, listen: false);
+    // final uemail = emaild.emaildata;
+    //final restaurant =
+    // final rest = Provider.of<Restaurants>(context, listen: false).rest;
+
+    // final _restaurant = ModalRoute.of(context).settings.arguments as Restaurant;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(rest['name']),
+        title: const Text("Profile"),
+        centerTitle: true,
       ),
-      body: Stack(
-        children: [
-          SafeArea(
-            child: Container(
-              height: Helper.getScreenHeight(context),
-              width: Helper.getScreenWidth(context),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ClipOval(
-                        child: Stack(
+      body: m == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Stack(
+              children: [
+                SafeArea(
+                  child: SizedBox(
+                    height: Helper.getScreenHeight(context),
+                    width: Helper.getScreenWidth(context),
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Column(
                           children: [
-                            Container(
-                              height: 150,
-                              width: 150,
-                              child: Image.asset(
-                                "images/profile.png",
-                                fit: BoxFit.cover,
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            ClipOval(
+                              child: Stack(
+                                children: [
+                                  SizedBox(
+                                    height: 150,
+                                    width: 150,
+                                    child: Image(
+                                      fit: BoxFit.cover,
+                                      image:
+                                          imageFromBase64String(m['imageUrl'])
+                                              .image,
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 0,
+                                    child: Container(
+                                      height: 30,
+                                      width: 150,
+                                      color: Colors.black.withOpacity(0.6),
+                                      child: Image.asset("images/camera.png"),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                            Positioned(
-                              bottom: 0,
-                              child: Container(
-                                height: 30,
-                                width: 150,
-                                color: Colors.black.withOpacity(0.6),
-                                child: Image.asset("images/camera.png"),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.edit, //color: Colors.deepOrange,
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                GestureDetector(
+                                  child: const Text(
+                                    "Edit Profile",
+                                    style: TextStyle(color: AppColor.orange),
+                                  ),
+                                  onTap: () => Navigator.of(context)
+                                      .pushNamed(HomePage.routeName),
+                                ),
+                              ],
+                            ),
+                            // const Center(
+                            //   child: Text('Your profile'),
+                            // ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            CustomFormImput(
+                              label: "Name",
+                              value: "${m['name']}",
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            CustomFormImput(
+                              label: "Email",
+                              value: emaild,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            CustomFormImput(
+                              label: "Mobile No",
+                              value: m['phone'],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            CustomFormImput(
+                              label: "Address",
+                              value: m['address'],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            // const CustomFormImput(
+                            //   label: "Password",
+                            //   value: "Emilia Clarke",
+                            //   isPassword: true,
+                            // ),
+                            // const SizedBox(
+                            //   height: 20,
+                            // ),
+                            // const CustomFormImput(
+                            //   label: "Confirm Password",
+                            //   value: "Emilia Clarke",
+                            //   isPassword: true,
+                            // ),
+                            // const SizedBox(
+                            //   height: 20,
+                            // ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            SizedBox(
+                              height: 50,
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("Back"),
                               ),
                             )
                           ],
                         ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.edit, //color: Colors.deepOrange,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          GestureDetector(
-                            child: Text(
-                              "Edit Profile",
-                              style: TextStyle(color: AppColor.orange),
-                            ),
-                            onTap: () => Navigator.of(context)
-                                .pushNamed(HomePage.routeName),
-                          ),
-                        ],
-                      ),
-                      Center(
-                        child: Text('Your profile'),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      CustomFormImput(
-                        label: "Name",
-                        value: "Emilia Clarke",
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomFormImput(
-                        label: "Email",
-                        value: "emiliaclarke@email.com",
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomFormImput(
-                        label: "Mobile No",
-                        value: "emiliaclarke@email.com",
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomFormImput(
-                        label: "Address",
-                        value: "No 23, 6th Lane, Colombo 03",
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomFormImput(
-                        label: "Password",
-                        value: "Emilia Clarke",
-                        isPassword: true,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      CustomFormImput(
-                        label: "Confirm Password",
-                        value: "Emilia Clarke",
-                        isPassword: true,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        height: 50,
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Save"),
-                        ),
-                      )
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
@@ -182,7 +224,7 @@ class CustomFormImput extends StatelessWidget {
       width: double.infinity,
       height: 50,
       padding: const EdgeInsets.only(left: 40),
-      decoration: ShapeDecoration(
+      decoration: const ShapeDecoration(
         shape: StadiumBorder(),
         color: AppColor.placeholderBg,
       ),
@@ -197,7 +239,7 @@ class CustomFormImput extends StatelessWidget {
         ),
         obscureText: _isPassword,
         initialValue: _value,
-        style: TextStyle(
+        style: const TextStyle(
           fontSize: 14,
         ),
       ),
